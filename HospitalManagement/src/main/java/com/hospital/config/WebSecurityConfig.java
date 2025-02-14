d169af4fc38f5c3873f7ae55a53ad4802f48ab1a
gramredd <gudla-harshit.ram-reddy@capgemini.com>
2025-02-14 13:46:39 +0530

    testing

--- HospitalManagement/src/main/java/com/hospital/config/WebSecurityConfig.java
+++ HospitalManagement/src/main/java/com/hospital/config/WebSecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.hospital.security.AuthEntryPointJwt;
 import com.hospital.security.AuthTokenFilter;
 import com.hospital.services.impl.UserDetailsServiceImpl;
 
